5e2c06e0f1a39653d166e37574435c7b8782d79d
GarcesSebastian <sebastiangarces152@gmail.com>
2026-02-22 01:04:17 -0500

    feat: Initialize Express server with a basic endpoint and SocketService.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import { config } from "dotenv";
 import { SocketService } from "./managers/_socket/index.js";
+import type { Request, Response } from "express";
 
 config();
 
@@ -9,6 +10,12 @@ const app = express();
 const server = createServer(app);
 
 const PORT = process.env.PORT || 4000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Server is running"
+    })
+})
 
 server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
